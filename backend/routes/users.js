9f3a1833ffe200f6cdbaf255dcb1f24498501438
Jonathan Quah <quahjieren@gmail.com>
2019-09-18 22:00:55 +0800

    First commit

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,3 +1,29 @@
 // Require router
-const router = require('express').Router()
-let User = require('')
+const router = require('express').Router();
+
+// Require user model
+let User = require('../models/user.model');
+
+// Create user home url
+router.route('/').get(
+    (req, res) => {
+        User.find()  // fetch users from mongodb database
+            .then(users => res.json(users))
+            .catch(err => res.status(400).json('Error: ' + err));
+    }
+);
+
+// Create user add url
+router.route('/add').post( 
+    (req, res) => {
+        const username = req.body.username;
+        const newUser = new User({username});  // create new instance of user using the username
+
+        newUser.save()
+            .then(() => res.json('User added!'))
+            .catch(() => res.status(400).json('Error: ' + err))
+    }
+);
+
+// Exporting router
+module.exports = router;
