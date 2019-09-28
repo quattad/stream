1e7c51e26164c802bfa4ed59f63ec667e8eb42e8
quahjieren@gmail.com <quahjieren@gmail.com>
2019-09-28 09:14:45 +0800

    Changed server.js to run on port 5000 instead of 3000

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 // Middleware
 app.use(cors())
