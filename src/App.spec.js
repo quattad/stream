8efc3f0cf3d8a2c00aca39758fb5708aa72f506a
quahjieren@gmail.com <quahjieren@gmail.com>
2019-09-28 10:13:15 +0800

    Configure correct importing of enzyme

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../../src/App.js';
+import App from './App.js';
 
 describe('App', () => {
     it('renders hello world', () => {
         const wrapper = shallow(<App />);
-        expect(wrapper.find('header').text()).toContain('Learn React');
+        expect(wrapper.find('Navbar').exists());
     });
 });
