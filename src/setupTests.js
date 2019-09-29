3445404fd27c9070f09e549eee902b43ca63a657
quahjieren@gmail.com <quahjieren@gmail.com>
2019-09-30 00:31:49 +0800

    Minor changes to frontend testing files to print msgs to console

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,9 +2,9 @@
  * Runs before the execution of every test file
  */
 
+console.log('\n Run setupTests... \n')
+
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16'
 
 Enzyme.configure({ adapter: new Adapter() });
-
-console.log('\n setupTests.js run! \n')
