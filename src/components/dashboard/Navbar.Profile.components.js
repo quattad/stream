1954b06c319c04fd43c97d7b2f5690aa3d0f9693
quahjieren@gmail.com <quahjieren@gmail.com>
2019-11-22 08:11:31 +0800

    Initial commit of Navbar.Profile.components.js

---
+++ src/components/dashboard/Navbar.Profile.components.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {useState} from 'react'
+
+// Import required reactstrap components
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Button, Collapse, NavbarBrand, Navbar, NavItem, NavLink, Nav, Container } from 'reactstrap';
+
+function NavbarProfile () {
+    // Define functions, hooks
+
+    return {
+        <React.Fragment></>
+    }
+};
+
+export default NavbarProfile
