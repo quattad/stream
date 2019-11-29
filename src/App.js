6e0af2f188bb07307f6ff5adab081f20205d209d
quahjieren@gmail.com <quahjieren@gmail.com>
2019-11-29 10:31:16 +0800

    Minor refactoring for App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 // import logo from './logo.svg';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
@@ -23,6 +24,7 @@ import {authInitialState, authReducer} from "./services/AuthReducer"
 
 function App() {
   const [state, dispatch] = React.useReducer(authReducer, authInitialState)
+
   return (
     // Map url paths to diff. components to load on page
     <AuthContext.Provider value={{
