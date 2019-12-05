de148482845bb06530dbd8f4d61afdf0f3998aec
quahjieren@gmail.com <quahjieren@gmail.com>
2019-12-05 11:58:36 +0800

    Added functionality for rendering projects on dashboard; message if no projects

--- src/components/dashboard/ProjectsTable.components.js
+++ src/components/dashboard/ProjectsTable.components.js
@@ -7,26 +7,23 @@ import ProjectsTableHeader from './ProjectsTableHeader.components'
 import ProjectsTableButton from "./ProjectsTableButton.components"
 import ProjectsTableRow from "./ProjectsTableRow.components"
 
-import {useDashboardContext} from "../DashboardPrivate.components" 
+import {useDashboardContext} from "../DashboardPrivate.components"
 
 function ProjectsTable () {
-    const dbContext = useDashboardContext();
-    let checkProjectExists = false
-    console.log(dbContext.userState.projects.length)
 
+    const dbContext = useDashboardContext();
+    
     const renderedProjects = (() => {
-        if (dbContext.userState.projects && dbContext.userState.projects > 0) {
-            checkProjectExists = true
+        if (dbContext.userState.projects && dbContext.userState.projects.length > 0) {
             let projects = dbContext.userState.projects
-            return projects.map(
-                project =>
-                <div className="row">
-                    <Container>
-                        <ProjectsTableRow project={project} />
-                    </Container>
-                </div>
+            return projects.map(project =>
+            <div className="row">
+                <Container><ProjectsTableRow project={project} /></Container>
+            </div>
             )
-        } 
+        } else {
+            return <p>You currently have no projects.</p>
+        }
     })();
 
     return (
@@ -37,7 +34,7 @@ function ProjectsTable () {
             <div className="float-right"><ProjectsTableButton /></div>
         </Container>
     </div>
-    {checkProjectExists ? (renderedProjects) : (<p>You have no projects.</p>)}
+    {renderedProjects}
     </>
     )
 }
