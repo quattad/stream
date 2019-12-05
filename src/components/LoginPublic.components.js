ca1405cb22599c7a5b6f6d697ae94c0d072fc8b3
quahjieren@gmail.com <quahjieren@gmail.com>
2019-12-05 11:54:58 +0800

    Added form validation messages to show based on backend response message; basic refactoring

--- src/components/LoginPublic.components.js
+++ src/components/LoginPublic.components.js
@@ -1,58 +1,94 @@
 import React from "react";
 import {Redirect} from "react-router"
+import axios from "axios";
 
-// reactstrap components
-import {Button,Card,CardBody,CardFooter,Form,Input,InputGroupAddon,InputGroupText,InputGroup,Container,Col
-} from "reactstrap";
+import { OverlayTrigger, UncontrolledPopover, PopoverBody, FormFeedback, FormGroup, Button, Card, CardBody, CardFooter, Form, Input, InputGroupAddon, InputGroupText, InputGroup, Container,Col } from "reactstrap";
 
-// core components
+// Import child components
 import Navbar from "./Navbar.components";
 import TransparentFooter from "./TransparentFooter";
-import axios from "axios";
 
 // import useAuth functions to set authState
 import {useAuthContext} from '../services/AuthReducer'
 
-function LoginPage() {
+// define email and password regexes
+const emailRegex = RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)
+const passwordRegex = RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/)
 
-  // Hook to fetch state object passed down from parent App component
-  // This is run everytime there is a change in SPA e.g. change in form field
+function LoginPage () {
   const auth = useAuthContext()
 
-  // Create state to store entered username and pw
+  // FORM STATE HOOKS
   const [emailState, setEmailState] = React.useState("");
+  const onChangeEmail = (e) => {setEmailState(e.target.value)};
+
   const [passwordState, setPasswordState] = React.useState("");
-  
+  const onChangePassword = (e) => {setPasswordState(e.target.value)};
+
+  // FORM FUNCTIONALITY HOOKS
+  const[showPassword, setShowPassword] = React.useState("password");
+  const onShowPassword = () => {setShowPassword(!showPassword)};
+
+  const[showPasswordButton, setShowPasswordButton] = React.useState("Show");
+  const onShowPasswordButton = () => {setShowPasswordButton(!showPasswordButton)};
+
+  // FORM FUNCTIONALITY HOOKS
   const [emailFocus, setEmailFocus] = React.useState(false);
   const [passwordFocus, setPasswordFocus] = React.useState(false);
+  const [emailIsValid, setEmailIsValid] = React.useState(false);
+  const [emailHasInput, setEmailHasInput] = React.useState(false)
+  const checkEmailIfValid = (e) => {
+    e.preventDefault();
 
-  // Define form save functions
-  const onChangeEmail = (e) => {
-    setEmailState(e.target.value)
-  };
-  const onChangePassword = (e) => {
-    setPasswordState(e.target.value)
-  };
-  const onShowPassword = (e) => {
-    setShowPassword(!showPassword)
-  };
-  const onShowPasswordButton = (e) => {
-    setShowPasswordButton(!showPasswordButton)
+    if (e.target.value.length > 0) {
+      setEmailHasInput(true);
+      
+      if (emailRegex.test(e.target.value)) {
+        setEmailIsValid(true);
+        setEmailHasInput(false); // To prevent override of valid field
+      } else {
+        setEmailIsValid(false);
+      }
+
+    } else {
+      setEmailHasInput(false);
+      setEmailIsValid(false);
+    }
   };
 
-  // Additional form features
+  const [passwordIsValid, setPasswordIsValid] = React.useState(false);
+  const [passwordHasInput, setPasswordHasInput] = React.useState(false)
+  const checkPasswordIfValid = (e) => {
+    e.preventDefault();
+
+    if (e.target.value.length > 0) {
+      setPasswordHasInput(true);
+      
+      if (passwordRegex.test(e.target.value)) {
+        setPasswordIsValid(true);
+        setPasswordHasInput(false); // To prevent override of valid field
+      } else {
+        setPasswordIsValid(false);
+      }
+
+    } else {
+      setPasswordHasInput(false);
+      setPasswordIsValid(false);
+    }
+  };
+
+  const [showEmailPopover, setShowEmailPopover] = React.useState(false)
+  const [emailPopoverMsg, setEmailPopoverMsg] = React.useState("")
+
+  const [showPasswordPopover, setShowPasswordPopover] = React.useState(false)
+  const [passwordPopoverMsg, setPasswordPopoverMsg] = React.useState("")
+
+  // REDIRECT HOOKS
   const[fireRedirectHome, setFireRedirectHome] = React.useState(false)
+  const onFireRedirectHome = () => {setFireRedirectHome(true)}
+
   const[fireRedirectRegister, setFireRedirectRegister] = React.useState(false)
-  const[showPassword, setShowPassword] = React.useState("password")
-  const[showPasswordButton, setShowPasswordButton] = React.useState("Show")
-
-  const onFireRedirectHome = (e) => {
-    setFireRedirectHome(true)
-  }
-
-  const onFireRedirectRegister = (e) => {
-    setFireRedirectRegister(true)
-  }
+  const onFireRedirectRegister = () => {setFireRedirectRegister(true)}
 
   // Define login function
   const onSubmitLogin = async (e) => {
@@ -66,16 +102,34 @@ function LoginPage() {
       });
 
       if (!res.data.error) {
-        auth.handleLogin();
+        await auth.handleLogin();
         onFireRedirectHome(e);
 
     }} catch (err) {
     /** Error handling for login issues */
-    if (err.response.data.description === "EMAIL_FIELD_EMPTY") {}
-    else if (err.response.data.description === "PASSWORD_FIELD_EMPTY") {}
-    else if (err.response.data.description === "USER_NOT_FOUND") {}
-    else if (err.response.data.description === "PASSWORD_DOES_NOT_MATCH") {}
-    else {throw new Error("Uncaught ")}
+    if (err.response.data.description === "EMAIL_FIELD_EMPTY") {
+      setShowEmailPopover(true)
+      setShowPasswordPopover(false)
+      setEmailPopoverMsg("Email field is empty.")
+    }
+    else if (err.response.data.description === "PASSWORD_FIELD_EMPTY") {
+      setShowPasswordPopover(true)
+      setShowEmailPopover(false)
+      setPasswordPopoverMsg("Password field is empty.")
+    }
+    else if (err.response.data.description === "USER_NOT_FOUND") {
+      setShowEmailPopover(true)
+      setShowPasswordPopover(false)
+      setEmailPopoverMsg("User was not found.")
+    }
+    else if (err.response.data.description === "PASSWORD_DOES_NOT_MATCH") {
+      setShowPasswordPopover(true)
+      setShowEmailPopover(false)
+      setPasswordPopoverMsg("Incorrect password.")
+    }
+    else {
+      throw new Error("Uncaught error: " + err)
+    }
   }
 };
 
@@ -85,6 +139,7 @@ function LoginPage() {
     document.documentElement.classList.remove("nav-open");
     window.scrollTo(0, 0);
     document.body.scrollTop = 0;
+
     return function cleanup() {
       document.body.classList.remove("login-page");
       document.body.classList.remove("sidebar-collapse");
@@ -96,12 +151,7 @@ function LoginPage() {
       {fireRedirectHome && <Redirect to='/dashboard'> push={true} </Redirect>}
       {fireRedirectRegister && <Redirect to='/register'> push={true}</Redirect>}
       <div className="page-header clear-filter" filter-color="blue">
-        <div
-          className="page-header-image"
-          style={{
-            backgroundImage: "url(" + require("../assets/img/login.jpg") + ")"
-          }}
-        ></div>
+        <div className="page-header-image" style={{backgroundImage: "url(" + require("../assets/img/login.jpg") + ")"}}></div>
         <div className="content">
           <Container>
             <Col className="ml-auto mr-auto" md="4">
@@ -111,76 +161,47 @@ function LoginPage() {
                     <h3 className="h3 seo">Welcome back to Stream.</h3>
                   </Container>
                   <CardBody>
-                    <InputGroup
-                      className={
-                        "no-border input-lg" + (emailFocus ? " input-group-focus" : "")
-                      }>
+                    <FormGroup className={"no-border input-lg" + (emailFocus ? " input-group-focus" : "")}>
+                      <Input id="emailField" 
+                      valid={emailIsValid} invalid={emailHasInput}
+                      placeholder="Email"
+                      onChange={(e)=>{onChangeEmail(e); checkEmailIfValid(e)}} 
+                      onFocus={() => setEmailFocus(true)} 
+                      onBlur={() => setEmailFocus(false)}>
+                      </Input><FormFeedback>Invalid email.</FormFeedback>
+                      <UncontrolledPopover trigger="focus" toggle={()=>{setShowEmailPopover(false)}} placement="right" isOpen={showEmailPopover} target="emailField">
+                        <PopoverBody>{emailPopoverMsg}</PopoverBody>
+                      </UncontrolledPopover>
+                    </FormGroup>
+                    <FormGroup className={"no-border input-lg" + (passwordFocus ? " input-group-focus" : "")}>
                       <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="now-ui-icons users_circle-08"></i>
-                        </InputGroupText>
                       </InputGroupAddon>
-                      <Input
-                        placeholder="Email"
-                        type="text"
-                        onChange={(e)=>onChangeEmail(e)}
-                        onFocus={() => setEmailFocus(true)}
-                        onBlur={() => setEmailFocus(false)}
-                      ></Input>
-                    </InputGroup>
-                    <InputGroup
-                      className={
-                        "no-border input-lg" +
-                        (passwordFocus ? " input-group-focus" : "")
-                      }
-                    >
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="now-ui-icons ui-1_lock-circle-open"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input
-                        placeholder="Password"
-                        type={(showPassword ? "password": "text")}
-                        onChange={(e)=>onChangePassword(e)}
-                        onFocus={() => setPasswordFocus(true)}
-                        onBlur={() => setPasswordFocus(false)}
-                      ></Input>
-                    </InputGroup>
+                      <Input id="passwordField" 
+                      valid={passwordIsValid} invalid={passwordHasInput} 
+                      placeholder="Password"
+                      type={(showPassword ? "password": "text")}
+                      onChange={(e)=>{onChangePassword(e); checkPasswordIfValid(e);}}
+                      onFocus={() => setPasswordFocus(true)}
+                      onBlur={() => setPasswordFocus(false)}
+                      ></Input><FormFeedback>Password must be between 8 to 20 characters, 1 uppercase and 1 lowercase letter.</FormFeedback>
+                      <UncontrolledPopover trigger="click" toggle={()=>{setShowPasswordPopover(false)}}placement="right" isOpen={showPasswordPopover} target="passwordField">
+                        <PopoverBody>{passwordPopoverMsg}</PopoverBody>
+                      </UncontrolledPopover>
+                    </FormGroup>
                     <div className="col-md-auto">
-                      <Button 
-                        outline 
-                        className="btn-round"
-                        size="md"
-                        onClick={(e) => {
+                      <Button outline className="btn-round" size="md" onClick={(e) => {
                           onShowPassword(e)
                           onShowPasswordButton(e)
-                        }}>
-                          {(showPasswordButton) ? "Show Password":"Hide Password"}
-                      </Button>
+                        }}>{(showPasswordButton) ? "Show Password":"Hide Password"}</Button>
                     </div>
                   </CardBody>
                   <CardFooter className="text-center">
-                    <Button
-                      outline
-                      block
-                      className="btn-round"
-                      onClick={(e) => {
-                        onSubmitLogin(e)
-                      }}
-                      size="lg"
-                    >
-                      Login
-                    </Button>
-                    <div className="pull-left">
-                      <h6>
-                      <a
-                      className="link"
-                      href="/register"
-                      onClick={() => onFireRedirectRegister()}
-                      >
-                        Register
-                        </a></h6>
+                    <Button outline block className="btn-round" size="lg" onClick={(e) => {
+                      onSubmitLogin(e)
+                    }}>Login</Button>
+                    <div className="pull-left"><h6><a className="link" href="/register" onClick={
+                      () => onFireRedirectRegister()
+                    }>Register</a></h6>
                     </div>
                   </CardFooter>
                 </Form>
