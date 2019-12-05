29b7e51406922ce655998f700ec768378e9fec94
quahjieren@gmail.com <quahjieren@gmail.com>
2019-12-05 11:56:22 +0800

    Updated loading of username in button, change from promise to async await; issue with memory leak to fix

--- src/components/dashboard/Navbar.Profile.components.js
+++ src/components/dashboard/Navbar.Profile.components.js
@@ -1,63 +1,54 @@
 import React from 'react'
-import {useState} from 'react'
-
 import {Redirect} from 'react-router'
-
 import axios from "axios";
 
-// Import required reactstrap components
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Button, Collapse, NavLink } from 'reactstrap';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, NavLink } from 'reactstrap';
 
-// import useAuth functions to set authState
+// Import authContext to check for authentication state; navbarContext to fetch username to render
 import {useAuthContext} from '../../services/AuthReducer'
+import {useNavbarContext} from "../Navbar.components"
 
 function NavbarProfile () {
-    // Import auth for authentication
-    const auth = useAuthContext(); 
+    const auth = useAuthContext();
+    const navbarContext = useNavbarContext();
 
-    // Define functions, hooks
+    // FUNCTIONAL HOOKS
     const [dropDownState, setDropDownState] = React.useState(false)
     const onFireDropDown = () => {setDropDownState(!dropDownState)}
-    
+
+    // REDIRECT HOOKS
     const [fireRedirectProfile, setFireRedirectProfile] = React.useState(false)
     const onFireRedirectProfile = () => setFireRedirectProfile(true)
-
-    // Set redirect state if user logs out
     const [fireRedirectHome, setFireRedirectHome] = React.useState(false)
-    const onFireRedirectHome = (e) => {
-        setFireRedirectHome(true)
-      }
+    const onFireRedirectHome = () => {setFireRedirectHome(true)}
 
     // Define logout function
-    const onSubmitLogout = (e) => {
-        axios.post('http://localhost:5000/users/logout', {}, {withCredentials: true})
-            .then((res) => {
-                if (!res.data.error) {
-                    auth.handleLogout()
-                    onFireRedirectHome(e)
-                }
-            })
-            .catch(err => {throw new Error(err)})
+    const onSubmitLogout = async (e) => {
+        try {
+            const res = await axios.post('http://localhost:5000/users/logout', {}, {withCredentials: true});
+            if (!res.data.error) {
+                auth.handleLogout();
+                onFireRedirectHome(e)
+            }
+        } catch (err) {
+            throw new Error(err)
         }
+    };
 
     return (
         <>
         {fireRedirectProfile && <Redirect to="/profile"> push={true} </Redirect>}
         {fireRedirectHome && <Redirect to="/"> push={true} </Redirect>}
         <NavLink>
-        <Dropdown hidden={!auth.state.isAuthenticated} isOpen={dropDownState} toggle={onFireDropDown}>
-            <DropdownToggle 
-            outline 
-            className="btn-dropdown btn-round" 
-            caret
-            >
-                <p>Jonathan Quah</p>
-            </DropdownToggle>
-            <DropdownMenu>
-                <DropdownItem><div onClick = {() => {onFireRedirectProfile()}}>View Profile</div></DropdownItem>
-                <DropdownItem><div onClick = {() => {onSubmitLogout()}}>Logout</div></DropdownItem>
-            </DropdownMenu>
-        </Dropdown>
+            <Dropdown hidden={!auth.state.isAuthenticated} isOpen={dropDownState} toggle={onFireDropDown}>
+                <DropdownToggle outline className="btn-dropdown btn-round" caret>
+                    <p>{navbarContext.userState.username}</p>
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem><div onClick = {() => {onFireRedirectProfile()}}>View Profile</div></DropdownItem>
+                        <DropdownItem><div onClick = {() => {onSubmitLogout()}}>Logout</div></DropdownItem>
+                    </DropdownMenu>
+            </Dropdown>
         </NavLink>
         </>
     )
