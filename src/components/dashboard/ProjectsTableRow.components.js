12029ef107a9be4de91d30f0f0516a76e4088cd3
quahjieren@gmail.com <quahjieren@gmail.com>
2019-12-05 12:05:00 +0800

    Added modal for deleting project, axios POST request to delete project in db

--- src/components/dashboard/ProjectsTableRow.components.js
+++ src/components/dashboard/ProjectsTableRow.components.js
@@ -1,7 +1,36 @@
 import React from 'react'
-import {Button} from 'reactstrap'
+import {Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap'
+import axios from 'axios'
 
 function ProjectsTableRow (props) {
+    const [deleteModalState, setDeleteModalState] = React.useState(false)
+    const onChangeDeleteButton = () => {setDeleteModalState(!deleteModalState)}
+
+    const onFireReload = () => {
+        window.location.reload();
+    }
+
+    const onDeleteProject = async () => {
+        try {
+        const res = await axios.post('http://localhost:5000/projects/delete',
+        {
+            "name": props.project
+        },
+        {
+            withCredentials: true
+        });
+
+        if (!res.data.error) {
+            onChangeDeleteButton();
+            onFireReload();
+        }
+    } catch (err) {
+        // Add Alert here
+        console.log(err)
+        onFireReload();
+    }
+};
+
     return (
     <>
     <div className="float-left">
@@ -9,6 +38,16 @@ function ProjectsTableRow (props) {
     </div> 
     <div className="float-right" style={{paddingTop:"20px"}}>
         <Button outline className="btn-round" size="md" onClick={() => {}}>View</Button>
+        <Button outline className="btn-round" size="md" onClick={() => {}}>Edit</Button>
+        <Button outline className="btn-round" size="md" onClick={onChangeDeleteButton}>Delete</Button>
+        <Modal isOpen={deleteModalState} toggle={onChangeDeleteButton}>
+            <ModalHeader toggle={onChangeDeleteButton}>Delete Project</ModalHeader>
+            <ModalBody>Are you sure you would like to delete this project?</ModalBody>
+            <ModalFooter>
+                <Button color="primary" onClick={onDeleteProject}>Yes</Button>
+                <Button color="secondary" onClick={onChangeDeleteButton}>No</Button>
+            </ModalFooter>
+        </Modal>
     </div>  
     </>
     )
