0c03909cc66be4473fa79dec902c4f3c16cd8d6f
quahjieren@gmail.com <quahjieren@gmail.com>
2019-12-12 00:01:09 +0800

    Updated project CRUD to convert usernames in members and admins fields as ids

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -5,16 +5,19 @@ const {check, validationResult} = require('express-validator');
 
 // Import models
 const Project = require('../models/project.model');
+const User = require('../models/user.model');
 
 // Import authentication middleware with JWT
 const auth = require('./auth')
+
+// Import user services
+const {usernameToUserId} = require('../services/userServices');
 
 // Create functionality
 router.route('/add').post(auth,
     [
         check('name')
             .isLength({min: 5, max: 30})
-            // .custom(value => !/\s/.test(value))
             .withMessage('Spaces will be converted into hyphens'),
         check('description')
             .isLength({max: 100}),
@@ -32,36 +35,48 @@ router.route('/add').post(auth,
             return res.status(422).json({
                 errors: errors.array()
             });
-        }
-
-        const name = req.body.name.replace(/\s+/g, '-').toLowerCase()
-        const description = req.body.description
-        const creator = req.user._id
-        const members = req.body.members
-        const admins = req.body.admins
-        const features = [] // temp
-        const newProject = new Project({name, description, creator, members, admins, features})
+        };
 
         try {
-            await newProject.save();
+            const membersIdArray = await Promise.all(
+                req.body.members.map(async (member) => {
+                    return await usernameToUserId(member);
+                })
+                );
+
+            const adminsIdArray = await Promise.all(
+                req.body.admins.map(async (admin) => {
+                    return await usernameToUserId(admin);
+                })
+                );
+
+            const name = req.body.name.replace(/\s+/g, '-').toLowerCase();
+            const description = req.body.description;
+            const creator = req.user._id;
+            const members = membersIdArray;
+            const admins = adminsIdArray;
+            const newProject = new Project({name, description, creator, members, admins});
+
+            await newProject.save(); 
             req.user.projects = req.user.projects.concat(newProject.name);
             await req.user.save();
             res.status(200).send("PROJECT_ADDED");
-        } catch (err){
+
+        } catch (err) {
             res.status(400).send({
                 "err": "ProjectAddError",
                 "description": err.message
-            })
+            });
         }
     }
 );
 
-// Read functionality - find one project based on user and project name
-router.route('/find').get(auth,
+// Read functionality - find one project
+router.route('/find/:projectId').get(auth,
     async (req, res) => {
         try {
             const project = await Project.findOne({
-                "name": req.body.name,
+                "_id": req.params.projectId,
                 "members": req.user.username
             });
             res.status(200).send(project);
@@ -73,6 +88,188 @@ router.route('/find').get(auth,
         }
     }
 );
+
+// Update Functionality - Update project name
+router.route('/update/name/:projectId').post(auth,
+    [
+        check('name')
+            .isLength({min: 5, max: 30})
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(422).json({
+                errors: errors.array()
+            });
+        }
+
+        try {
+            conditions = {
+                "_id": req.params.projectId,
+                "members": req.user.username
+            };
+
+            options = {
+                upsert: true
+            };
+
+            projectToUpdate = {
+                "name": req.body.projectName
+            };
+
+            await Project.findOneAndUpdate(conditions, options, projectToUpdate);
+            res.status(200).send(
+                "PROJECT_NAME_UPDATED"
+                );
+        } catch (err) {
+            res.status(400).send({
+                "err": "ProjectNameUpdateError",
+                "description": err
+            });
+        };
+    });
+
+// Update Functionality - Update project description
+router.route('/update/description/:projectId').post(auth,
+    [
+        check('description')
+            .isLength({max: 100})
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(422).json({
+                errors: errors.array()
+            });
+        }
+
+        try {
+            conditions = {
+                "_id": req.params.projectId,
+                "members": req.user.username
+            };
+
+            options = {
+                upsert: true
+            };
+
+            projectToUpdate = {
+                "description": req.body.projectDescription
+            };
+
+            await Project.findOneAndUpdate(conditions, options, projectToUpdate);
+            res.status(200).send(
+                "PROJECT_DESCRIPTION_UPDATED"
+            );
+        } catch (err) {
+            res.status(400).send({
+                "err": "ProjectDescriptionUpdateError",
+                "description": err
+            });
+        };
+    });
+
+// Update Functionality - Update members
+router.route('/update/members/:projectId').post(auth,
+    [
+        check('members')
+            .isArray()
+            .isLength({min: 1, max: 10})
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(422).json({
+                errors: errors.array()
+            });
+        }
+
+        try {
+            conditions = {
+                "_id": req.params.projectId,
+                "members": req.user.username
+            };
+
+            options = {
+                upsert: true
+            };
+
+            let membersToAdd = await Promise.all(
+                req.body.members.map(async(member) => {
+                    return await usernameToUserId(member);
+                })
+                );
+
+            projectToUpdate = {
+                $push: {
+                    "members": membersToAdd
+                } 
+            };
+
+            await Project.findOneAndUpdate(conditions, options, projectToUpdate);
+            res.status(200).send(
+                "PROJECT_MEMBERS_UPDATED"
+            );
+        } catch (err) {
+            res.status(400).send({
+                "err": "ProjectMembersUpdateError",
+                "description": err
+            });
+        };
+    });
+
+// Update Functionality - Update admin
+router.route('/update/admins/:projectId').post(auth,
+    [
+        check('admins')
+            .isArray()
+            .isLength({min: 1})
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(422).json({
+                errors: errors.array()
+            });
+        }
+
+        try {
+            conditions = {
+                "_id": req.params.projectId,
+                "members": req.user.username
+            };
+
+            options = {
+                upsert: true
+            };
+
+            let adminsToAdd = await Promise.all(
+                req.body.admins.map(async(admin) => {
+                    return await usernameToUserId(admin)
+                })
+            );
+
+            projectToUpdate = {
+                $push: {
+                    "members": adminsToAdd
+                } 
+            };
+
+            await Project.findOneAndUpdate(conditions, options, projectToUpdate);
+            res.status(200).send(
+                "PROJECT_ADMINS_UPDATED"
+            );
+        } catch (err) {
+            res.status(400).send({
+                "err": "ProjectAdminsUpdateError",
+                "description": err
+            });
+        };
+    });
 
 // Delete functionality - delete single project based on user and project name
 router.route('/delete').post(auth,
@@ -88,7 +285,6 @@ router.route('/delete').post(auth,
             });
 
             await req.user.save()
-
             res.status(200).send("PROJECT_DELETED");
         } catch (err) {
             res.status(400).send({
@@ -97,108 +293,6 @@ router.route('/delete').post(auth,
             });
         }
     });
-
-// Update Functionality - Update project name
-// router.route('/:username/:name/update/name').post(
-//     [
-//         check('name')
-//             .isLength({min: 5, max: 30}),
-//     ],
-//     (req, res) => {
-//         const errors = validationResult(req);
-
-//         if (!errors.isEmpty()) {
-//             return res.status(422).json({
-//                 errors: errors.array()
-//             });
-//         }
-
-//         Project.findOne({"name": req.params.name}, (err, doc) => {
-//             doc.name = req.body.name
-
-//             doc.save()
-//                 .then(() => {res.json('Project added')})
-//                 .catch((err) => {res.status(400).json('Error' + err)})
-//         })
-//     });
-
-// Update Functionality - Update project description
-// router.route('/:username/:name/update/description').post(
-//     [
-//         check('description')
-//             .isLength({max: 100}),
-//     ],
-//     (req, res) => {
-//         const errors = validationResult(req);
-
-//         if (!errors.isEmpty()) {
-//             return res.status(422).json({
-//                 errors: errors.array()
-//             });
-//         }
-
-//         Project.findOne({"name": req.params.name}, (err, doc) => {
-//             doc.description = req.body.description
-
-//             doc.save()
-//                 .then(() => {res.json('Updated project description')})
-//                 .catch((err) => {res.status(400).json('Error' + err)})
-//         })
-//     });
-
-// Update Functionality - Update members
-// router.route('/:username/:name/update/user').post(
-//     [
-//         check('members')
-//             .isArray()
-//             .isLength({min: 1, max: 10})
-//     ],
-//     (req, res) => {
-//         const errors = validationResult(req);
-
-//         if (!errors.isEmpty()) {
-//             return res.status(422).json({
-//                 errors: errors.array()
-//             });
-//         }
-
-//         Project.findOne({"name": req.params.name}, (err, doc) => {
-//             for (let i = 0; i < req.body.members.length; i++) {
-//                 doc.members.push(req.body.members[i])
-//         }
-
-//             doc.save()
-//                 .then(() => {res.json('Updated project members')})
-//                 .catch((err) => {res.status(400).json('Error' + err)})
-//         })
-//     });
-
-// Update Functionality - Update admin
-// router.route('/:username/:name/update/admin').post(
-//     [
-//         check('admins')
-//             .isArray()
-//             .isLength({min: 1, max: 10})
-//     ],
-//     (req, res) => {
-//         const errors = validationResult(req);
-
-//         if (!errors.isEmpty()) {
-//             return res.status(422).json({
-//                 errors: errors.array()
-//             });
-//         }
-
-//         Project.findOne({"name": req.params.name}, (err, doc) => {
-//             for (let i = 0; i < req.body.admins.length; i++) {
-//                 doc.admins.push(req.body.admins[i])
-//         }
-
-//             doc.save()
-//                 .then(() => {res.json('Updated project admins')})
-//                 .catch((err) => {res.status(400).json('Error' + err)})
-//         })
-//     });
 
 // Exporting router
 module.exports = router;
