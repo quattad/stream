6a7dbc0548928d3f964fb16b920e28b7c99d1051
quahjieren@gmail.com <quahjieren@gmail.com>
2019-12-11 23:59:16 +0800

    Added user id to request object alongside req.user and req.token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,24 +4,27 @@ const User = require('../models/user.model')
 
 const auth = async (req, res, next) => {
     try {
-        // Store current login token in 
-        const token = req.signedCookies.token
+        const token = req.signedCookies.token;
         
         // Verify token signature, returns payload if verified, returns nothing if not
-        const data = jwt.verify(token, process.env.JWT_KEY);
-        const user = await User.findOne({'_id': data.id, 'tokens.token':token})
+        const data = jwt.verify(token, process.env.JWT_KEY);;
+        const user = await User.findOne({'_id': data.id, 'tokens.token':token});
         
         if (!user) {
-            throw new Error ('USER_NOT_FOUND_WITH_SPECIFIED_TOKEN')
-        }
-        req.user = user
-        req.token = token
+            throw new Error ('USER_NOT_FOUND_WITH_SPECIFIED_TOKEN');
+        };
+
+        req.user = user;
+        req.token = token;
+        req.userId = user._id;
+
         next();
+        
     } catch (err) { 
         return res.status(401).send({
             "error": "ValidationError",
-            "description": err})
-        }
-}
+            "description": err});
+        };
+};
 
 module.exports = auth;
