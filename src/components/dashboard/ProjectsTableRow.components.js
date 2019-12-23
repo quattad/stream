259f6a08430de7d9bfc866bf6f65b7e809e6ac3c
quahjieren@gmail.com <quahjieren@gmail.com>
2019-12-24 00:16:06 +0800

    Minor refactoring, semicolon and env var; Update View button link to ProjectDView

--- src/components/dashboard/ProjectsTableRow.components.js
+++ src/components/dashboard/ProjectsTableRow.components.js
@@ -1,18 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 import {Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap'
 import axios from 'axios'
 
 function ProjectsTableRow (props) {
-    const [deleteModalState, setDeleteModalState] = React.useState(false)
-    const onChangeDeleteButton = () => {setDeleteModalState(!deleteModalState)}
+    const [deleteModalState, setDeleteModalState] = React.useState(false);
+    const onChangeDeleteButton = () => {setDeleteModalState(!deleteModalState)};
 
     const onFireReload = () => {
         window.location.reload();
-    }
+    };
+
+    const projectDViewUri = `/projects/${props.project}`;
 
     const onDeleteProject = async () => {
         try {
-            const res = await axios.post('http://localhost:5000/projects/delete',
+            const res = await axios.post(`${process.env.REACT_APP_BASE_SERVER_URL}/projects/delete`,
             {
                 "name": props.project
             },
@@ -29,14 +33,13 @@ function ProjectsTableRow (props) {
             onFireReload();
         }
     };
-
     return (
     <>
     <div className="float-left">
         <h4>{props.project}</h4>
     </div> 
     <div className="float-right" style={{paddingTop:"20px"}}>
-        <Button outline className="btn-round" size="md" onClick={() => {}}>View</Button>
+        <Link to={projectDViewUri}><Button outline className="btn-round" size="md">View</Button></Link>
         <Button outline className="btn-round" size="md" onClick={() => {}}>Edit</Button>
         <Button outline className="btn-round" size="md" onClick={onChangeDeleteButton}>Delete</Button>
         <Modal isOpen={deleteModalState} toggle={onChangeDeleteButton}>
