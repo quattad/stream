a87328522a53c536594370e4beb966b15b683187
quahjieren@gmail.com <quahjieren@gmail.com>
2019-12-24 00:21:38 +0800

    First commit project dashboard component

---
+++ src/components/projectDView/ProjectDViewProjectDashboard.components.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProjectDViewProjectDashboard() {
+    return(
+        <>
+        <h3>This is a b00tiful Project Dashboard!</h3>
+        </>
+    );
+}
+
+export default ProjectDViewProjectDashboard;
