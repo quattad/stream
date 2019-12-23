65efd0938660607a5967d3c47eca26df1d8051a7
quahjieren@gmail.com <quahjieren@gmail.com>
2019-12-23 11:17:10 +0800

    Minor refactoring of HomePublic

--- src/components/HomePublic.components.js
+++ src/components/HomePublic.components.js
@@ -1,6 +1,6 @@
-import React from "react";
-import {Redirect} from "react-router"
-import { Button, Container, Card, CardFooter, Form, Col } from "reactstrap";
+import React from 'react';
+import { Redirect } from 'react-router';
+import { Button, Container, Card, CardFooter, Form, Col } from 'reactstrap';
 
 // Import child components
 import TransparentFooter from './TransparentFooter'
