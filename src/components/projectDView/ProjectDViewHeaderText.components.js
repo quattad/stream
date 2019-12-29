305986b5529dc7b60db087f9d2f22514f4a4f8a0
quahjieren@gmail.com <quahjieren@gmail.com>
2019-12-29 13:25:47 +0800

    Added spinner to ProjectDViewHeaderText comp

--- src/components/projectDView/ProjectDViewHeaderText.components.js
+++ src/components/projectDView/ProjectDViewHeaderText.components.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Container, Col, Row } from 'reactstrap';
+import ProjectSpinner from "../ProjectSpinner.components";
 
 function ProjectDViewHeaderText(props) {
     return(
         <>
         <Container>
             <Col>
-            <Row><h2>{props.pdvContext.projectData.name}</h2></Row>
-            <Row><h5>{props.pdvContext.projectData.description}</h5></Row>
+            <Row><h2>{props.pdvContext.projectData.name ? props.pdvContext.projectData.name : <ProjectSpinner />}</h2></Row>
+            <Row><h5>{props.pdvContext.projectData.description ? props.pdvContext.projectData.description : <ProjectSpinner />}</h5></Row>
             </Col>
         </Container>
         </>
