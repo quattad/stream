2baa05e7978efcc9a6e89a042e1283f39e400270
quahjieren@gmail.com <quahjieren@gmail.com>
2019-12-29 13:22:19 +0800

    Shifted require .env

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 
 // Allow connection to db server
 const mongoose = require('mongoose');
-
-require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000
