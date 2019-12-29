06eb3054b3aef53ad024c16a017fd3e44d6b6201
quahjieren@gmail.com <quahjieren@gmail.com>
2019-12-29 13:27:02 +0800

    Added project spinner to WelcomeText

--- src/components/dashboard/WelcomeText.components.js
+++ src/components/dashboard/WelcomeText.components.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import {useDashboardContext} from "../DashboardPrivate.components" 
+import React from "react";
+import ProjectSpinner from "../ProjectSpinner.components";
+import { useDashboardContext } from "../DashboardPrivate.components";
 
 function WelcomeText () {
     const dbContext = useDashboardContext()
     return (
     <>
     <div className="float-left">
-        <h1>Welcome, {dbContext.userState.firstname} {dbContext.userState.lastname}</h1>
+        <h1>Welcome, {dbContext.userState.firstname ? dbContext.userState.firstname : <ProjectSpinner />} {dbContext.userState.lastname ? dbContext.userState.lastname : <ProjectSpinner />}</h1>
     </div> 
     </>
     )
