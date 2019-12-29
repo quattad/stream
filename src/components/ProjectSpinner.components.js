275d5a0b8810ed712135363407677aaaa4eb21f4
quahjieren@gmail.com <quahjieren@gmail.com>
2019-12-29 13:25:06 +0800

    First commit of ProjectSpinner component

---
+++ src/components/ProjectSpinner.components.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Spinner } from 'reactstrap';
+
+const ProjectSpinner = () => {
+    return(
+        <>
+        <Spinner type="grow" color="info" />
+        </>
+    )
+};
+
+export default ProjectSpinner;
